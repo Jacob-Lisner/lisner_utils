e8a439778279e69c7c63af0a25d982e585da35d7
Jacob-Lisner <jslisner@gmail.com>
2024-06-24 14:37:23 -0400

    Update lexicon_counter.py
    removed if main

--- lexicon_counter.py
+++ lexicon_counter.py
@@ -47,36 +47,36 @@ def main(
         big_word = pickle.load(f)
     lex_norm = big_word
     for cp in paper_folders:
-        if __name__ == '__main__':
-            pool = get_context("spawn").Pool(processes=processes)
-            for file in os.listdir(cp):
-                paper_name = file[:-4]
-                print("==============================")
-                print(paper_name)
-                count_path = cp+paper_name+'.txt'
-                for lex_name in lex_names:
-                    print("------------------------------")
-                    print(lex_name)
-                    if inds:
-                        fold = os.path.join(os.path.split(os.path.dirname(count_path))[1]+"_Inds_1Perc", lex_name)
-                    else:
-                        fold = os.path.join(os.path.split(os.path.dirname(count_path))[1], lex_name)
-                    folder = './lisner_utils/!Lexicon_Counts/'+fold
-                    if not os.path.exists(folder):
-                        os.mkdir(folder)
-                    lexicon = lx.load_master(lex_name)
-                    temp = partial(mpd.get_lex, types = types, lexicon=lexicon, tot=0, thresh=0, thresh_start=0, time_start = 0, lex_norm=lex_norm, type_check=False, ind_c=inds)
-                    #temp = partial(mpd.get_lex2, path = os.path.abspath(folder+'\\'+paper_name+'_'+lex_name+'_norm-big0'), types = types, lexicon=lexicon, tot=0, thresh=0, thresh_start=0, time_start = 0, lex_norm=lex_norm, type_check=False, ind_c=inds)
+        #if __name__ == '__main__':
+        pool = get_context("spawn").Pool(processes=processes)
+        for file in os.listdir(cp):
+            paper_name = file[:-4]
+            print("==============================")
+            print(paper_name)
+            count_path = cp+paper_name+'.txt'
+            for lex_name in lex_names:
+                print("------------------------------")
+                print(lex_name)
+                if inds:
+                    fold = os.path.join(os.path.split(os.path.dirname(count_path))[1]+"_Inds_1Perc", lex_name)
+                else:
+                    fold = os.path.join(os.path.split(os.path.dirname(count_path))[1], lex_name)
+                folder = './lisner_utils/!Lexicon_Counts/'+fold
+                if not os.path.exists(folder):
+                    os.mkdir(folder)
+                lexicon = lx.load_master(lex_name)
+                temp = partial(mpd.get_lex, types = types, lexicon=lexicon, tot=0, thresh=0, thresh_start=0, time_start = 0, lex_norm=lex_norm, type_check=False, ind_c=inds)
+                #temp = partial(mpd.get_lex2, path = os.path.abspath(folder+'\\'+paper_name+'_'+lex_name+'_norm-big0'), types = types, lexicon=lexicon, tot=0, thresh=0, thresh_start=0, time_start = 0, lex_norm=lex_norm, type_check=False, ind_c=inds)
 
-                    with open(folder+'/'+paper_name+'_'+lex_name+'.txt', 'w') as f:
-                        for paper in pool.imap_unordered(temp, 
-                                                        count_intake(os.path.abspath(count_path)), 
-                                                        chunksize=1000):
-                            if paper is not None:
-                                f.write(str(paper) + "\n")
-                                
-            pool.close()
-            pool.join()
+                with open(folder+'/'+paper_name+'_'+lex_name+'.txt', 'w') as f:
+                    for paper in pool.imap_unordered(temp, 
+                                                    count_intake(os.path.abspath(count_path)), 
+                                                    chunksize=1000):
+                        if paper is not None:
+                            f.write(str(paper) + "\n")
+                            
+        pool.close()
+        pool.join()
 
 if __name__ == "__main__":
     main()

02d8a376640b19b154b8fc1358fdfc8897997a96
Jacob-Lisner <jslisner@gmail.com>
2024-06-24 12:16:57 -0400

    Fixed paths in loc_refs

--- loc_refs.py
+++ loc_refs.py
@@ -2,16 +2,17 @@
 #reference json
 import json
 import pathlib
+import os
 
 def main(ref):
-    with open(str(pathlib.Path(__file__).parent.resolve())+"\\refs\\loc_refs.json", "r") as f:
+    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "//refs//loc_refs.json"), "r") as f:
         j = json.loads(f.read())
     return j[ref]
 def update(ref, value):
-    with open(str(pathlib.Path(__file__).parent.resolve())+"\\refs\\loc_refs.json", "r") as f:
+    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "//refs//loc_refs.json"), "r") as f:
         j = json.loads(f.read())
     j[ref] = value
-    with open(str(pathlib.Path(__file__).parent.resolve())+"\\refs\\loc_refs.json", "w") as f:
+    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "//refs//loc_refs.json"), "w") as f:
         json.dump(j, f)
 if __name__ == "__main__":
     main()

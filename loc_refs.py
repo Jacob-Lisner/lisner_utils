f30fbedab4ce541415ab38ffc96a21dafce15c9e
Jacob-Lisner <jslisner@gmail.com>
2024-06-24 12:20:20 -0400

    Again

--- loc_refs.py
+++ loc_refs.py
@@ -5,14 +5,14 @@ import pathlib
 import os
 
 def main(ref):
-    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json"), "r") as f:
+    with open(os.path.abspath(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json")), "r") as f:
         j = json.loads(f.read())
     return j[ref]
 def update(ref, value):
-    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json"), "r") as f:
+    with open(os.path.abspath(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json")), "r") as f:
         j = json.loads(f.read())
     j[ref] = value
-    with open(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json"), "w") as f:
+    with open(os.path.abspath(os.path.join(str(pathlib.Path(__file__).parent.resolve()), "/refs/loc_refs.json")), "w") as f:
         json.dump(j, f)
 if __name__ == "__main__":
     main()
